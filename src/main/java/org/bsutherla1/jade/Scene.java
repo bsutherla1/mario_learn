4c8a5f5ae46b40cfdc3824bf156e6bc5924dd267
Brian Sutherland <bsutherla1@live.esu.edu>
2025-07-17 13:59:43 -0400

    Updated and cleaned up to use RenderBatching system

--- src/main/java/org/bsutherla1/jade/Scene.java
+++ src/main/java/org/bsutherla1/jade/Scene.java
@@ -1,10 +1,13 @@
 package org.bsutherla1.jade;
 
+import org.bsutherla1.renderer.Renderer;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public abstract class Scene {
 
+    protected Renderer renderer = new Renderer();
     protected Camera camera;    // Quick and dirty update to test Camera
     private boolean isRunning = false;
     protected List<GameObject> gameObjects = new ArrayList<>();
@@ -20,6 +23,7 @@ public abstract class Scene {
     public void start() {
         for (GameObject go : gameObjects) {
             go.start();
+            renderer.add(go);
         }
         isRunning = true;
     }
@@ -30,8 +34,13 @@ public abstract class Scene {
         } else {
             gameObjects.add(go);
             go.start();
+            renderer.add(go);
         }
     }
 
     public abstract void update(float dt);
+
+    public Camera camera() {
+        return camera;
+    }
 }
