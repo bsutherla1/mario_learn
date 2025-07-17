c012e93524c1179b7db9b841c7b2413b12d2e27f
Brian Sutherland <bsutherla1@live.esu.edu>
2025-07-17 10:46:35 -0400

    Initial Commit - Component Architecture

---
+++ src/main/java/org/bsutherla1/components/FontRenderer.java
@@ -0,0 +1,18 @@
+package org.bsutherla1.components;
+
+import org.bsutherla1.jade.Component;
+
+public class FontRenderer extends Component {
+
+    @Override
+    public void start() {
+        if (gameObject.getComponent(SpriteRenderer.class) != null) {
+            System.out.println("Found Font Renderer!");
+        }
+    }
+
+    @Override
+    public void update(float dt) {
+
+    }
+}
