36ee67f7841a1dabc5cb47dadca080167ab2514b
Brian Sutherland <bsutherla1@live.esu.edu>
2025-07-19 09:50:06 -0400

    Removed Time utility due to defects in time keeping.

--- src/main/java/org/bsutherla1/util/Time.java
+++
@@ -1,7 +0,0 @@
-package org.bsutherla1.util;
-
-public class Time {
-    public static float timeStarted = System.nanoTime();
-
-    public static float getTime() { return (float)((System.nanoTime() - timeStarted) * 1E-9); }
-}
