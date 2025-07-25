7eef826559df9a18b8c81619293caea27864dd38
Brian Sutherland <bsutherla1@live.esu.edu>
2025-07-25 16:41:48 -0400

    Added Spritesheet support

--- src/main/java/org/bsutherla1/util/AssetPool.java
+++ src/main/java/org/bsutherla1/util/AssetPool.java
@@ -1,5 +1,6 @@
 package org.bsutherla1.util;
 
+import org.bsutherla1.components.Spritesheet;
 import org.bsutherla1.renderer.Shader;
 import org.bsutherla1.renderer.Texture;
 
@@ -11,6 +12,7 @@ import java.util.Map;
 public class AssetPool {
     private static Map<String, Shader> shaders = new HashMap<>();
     private static Map<String, Texture> textures = new HashMap<>();
+    private static Map<String, Spritesheet> spritesheets = new HashMap<>();
 
     public static Shader getShader(String resourceName) {
         File file = new File(resourceName);
@@ -39,5 +41,22 @@ public class AssetPool {
                 return null;
             }
         }
+    }
+
+    public static void addSpritesheet(String resourceName, Spritesheet spritesheet) {
+        File file = new File(resourceName);
+        if (!AssetPool.spritesheets.containsKey(file.getAbsolutePath())) {
+            AssetPool.spritesheets.put(file.getAbsolutePath(), spritesheet);
+        }
+    }
+
+    public static Spritesheet getSpritesheet(String resourceName) {
+        File file = new File(resourceName);
+        if (!AssetPool.spritesheets.containsKey(file.getAbsolutePath())) {
+            assert false : "Error: Tried to access spritesheet '" + resourceName +
+                    "' and it has not been added to asset pool.";
+        }
+
+        return AssetPool.spritesheets.getOrDefault(file.getAbsolutePath(), null);
     }
 }
