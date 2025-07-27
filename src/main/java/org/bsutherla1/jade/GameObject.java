0973750ff968ad734cb110304f4b337e8efd8fa2
Brian Sutherland <bsutherla1@live.esu.edu>
2025-07-27 09:33:46 -0400

    Code clean-up

--- src/main/java/org/bsutherla1/jade/GameObject.java
+++ src/main/java/org/bsutherla1/jade/GameObject.java
@@ -61,8 +61,8 @@ public class GameObject {
     }
 
     public void start() {
-        for (int i = 0; i < components.size(); i++) {
-            components.get(i).start();
+        for (Component component : components) {
+            component.start();
         }
     }
 }
