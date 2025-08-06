ec268a354382ce0d1d3856c074778d43fc60aef2
Brian Sutherland <bsutherla1@live.esu.edu>
2025-08-06 15:10:14 -0400

    Created dirtyFlag support to know when to redraw sprite.

--- src/main/java/org/bsutherla1/components/SpriteRenderer.java
+++ src/main/java/org/bsutherla1/components/SpriteRenderer.java
@@ -1,6 +1,7 @@
 package org.bsutherla1.components;
 
 import org.bsutherla1.jade.Component;
+import org.bsutherla1.jade.Transform;
 import org.bsutherla1.renderer.Texture;
 import org.joml.Vector2f;
 import org.joml.Vector4f;
@@ -9,6 +10,9 @@ public class SpriteRenderer extends Component {
 
     private Vector4f color;
     private Sprite sprite;
+
+    private Transform lastTransform;
+    private boolean isDirty = true;
 
     public SpriteRenderer(Vector4f color) {
         this.color = color;
@@ -22,12 +26,15 @@ public class SpriteRenderer extends Component {
 
     @Override
     public void start() {
-
+        lastTransform = gameObject.transform.copy();
     }
 
     @Override
     public void update(float dt) {
-
+        if (!lastTransform.equals(gameObject.transform)) {
+            gameObject.transform.copy(lastTransform);
+            isDirty = true;
+        }
     }
 
     public Vector4f getColor() {
@@ -42,5 +49,25 @@ public class SpriteRenderer extends Component {
         // Texture Coordinates are arranged from upper right corner and rotates
         // clockwise around the sprite.
         return sprite.getTexCoords();
+    }
+
+    public void setSprite(Sprite sprite) {
+        this.sprite = sprite;
+        isDirty = true;
+    }
+
+    public void setColor(Vector4f color) {
+        if (!this.color.equals(color)) {
+            isDirty = true;
+            this.color.set(color);
+        }
+    }
+
+    public boolean isDirty() {
+        return isDirty;
+    }
+
+    public void setClean() {
+        isDirty = false;
     }
 }
