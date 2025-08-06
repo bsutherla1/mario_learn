c6899134ad1fb8d484a9888e27a242719309e826
Brian Sutherland <bsutherla1@live.esu.edu>
2025-08-06 15:09:21 -0400

    Added copy methods and overode equals method.

--- src/main/java/org/bsutherla1/jade/Transform.java
+++ src/main/java/org/bsutherla1/jade/Transform.java
@@ -23,4 +23,21 @@ public class Transform {
         this.position = position;
         this.scale = scale;
     }
+
+    public Transform copy() {
+        return new Transform(new Vector2f(this.position), new Vector2f(this.scale));
+    }
+
+    public void copy(Transform to) {
+        to.position.set(position);
+        to.scale.set(scale);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null) return false;
+        if (!(o instanceof Transform t)) return false;
+
+        return t.position.equals(position) && t.scale.equals(scale);
+    }
 }
